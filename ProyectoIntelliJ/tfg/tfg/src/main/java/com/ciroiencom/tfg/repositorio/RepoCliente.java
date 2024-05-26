ada424a720faed8d69a56b3bf232769a38be1bb6
ciroco49 <ciroiencom@hotmail.com>
2024-05-26 22:41:42 +0200

    Creación métodos en repositorios

--- ProyectoIntelliJ/tfg/tfg/src/main/java/com/ciroiencom/tfg/repositorio/RepoCliente.java
+++ ProyectoIntelliJ/tfg/tfg/src/main/java/com/ciroiencom/tfg/repositorio/RepoCliente.java
@@ -4,8 +4,8 @@ package com.ciroiencom.tfg.repositorio;
 import com.ciroiencom.tfg.modelo.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepoCliente extends JpaRepository<Cliente, String> {
-
-
-
+    List<Cliente> findByNombre(String nombre);
 }
--- ProyectoIntelliJ/tfg/tfg/src/main/java/com/ciroiencom/tfg/repositorio/RepoTener.java
+++ ProyectoIntelliJ/tfg/tfg/src/main/java/com/ciroiencom/tfg/repositorio/RepoTener.java
@@ -1,8 +1,13 @@
 package com.ciroiencom.tfg.repositorio;
 
+import com.ciroiencom.tfg.modelo.Cliente;
+import com.ciroiencom.tfg.modelo.DTO.TenerDTO;
 import com.ciroiencom.tfg.modelo.Tener;
 import com.ciroiencom.tfg.modelo.TenerID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepoTener extends JpaRepository<Tener, TenerID> {
+    List<Tener> findByDNICliente(Cliente cliente);
 }
