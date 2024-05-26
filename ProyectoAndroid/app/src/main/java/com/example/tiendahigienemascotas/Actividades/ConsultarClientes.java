966c7f0895c5b2ade35adf3f40249183b992eeb0
ciroco49 <ciroiencom@hotmail.com>
2024-05-26 22:37:49 +0200

    Implementación main consultas Clientes

---
+++ ProyectoAndroid/app/src/main/java/com/example/tiendahigienemascotas/Actividades/ConsultarClientes.java
@@ -0,0 +1,105 @@
+package com.example.tiendahigienemascotas.Actividades;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tiendahigienemascotas.AdaptadorClientes;
+import com.example.tiendahigienemascotas.CallBacks.ClientesCallBack;
+import com.example.tiendahigienemascotas.Controladores.ClienteController;
+import com.example.tiendahigienemascotas.Modelos.Cliente;
+import com.example.tiendahigienemascotas.R;
+import com.example.tiendahigienemascotas.Regex;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultarClientes extends AppCompatActivity implements ClientesCallBack {
+ListView listview_clientes;
+AdaptadorClientes adaptador;
+EditText filtroDNI_cliente;
+EditText filtroNombre_cliente;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consultar_clientes);
+
+        listview_clientes = findViewById(R.id.listView_consultarClientes);
+        filtroDNI_cliente = findViewById(R.id.filtroDNICliente);
+        filtroNombre_cliente = findViewById(R.id.filtroNombreCliente);
+
+        //Al entrar a esta pantalla, cargo todos los clientes
+        ClienteController.getClientes(this, this);
+
+        //Creo el listener para detectar clicks en clientes de la lista
+        listview_clientes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cliente cliente_seleccionado = (Cliente) parent.getItemAtPosition(position);
+
+                //Voy a la pantalla de info, enviando el DNI del cliente seleccionado para obtenerlo con una petición
+                Intent infoCliente = new Intent(ConsultarClientes.this, InfoCliente.class);
+                infoCliente.putExtra("DNI", cliente_seleccionado.getDNI());
+                startActivity(infoCliente);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onSuccessClientes(List<Cliente> array_clientes) {
+        if(listview_clientes.getAdapter() == null) {
+            adaptador = new AdaptadorClientes(this, array_clientes);
+            listview_clientes.setAdapter(adaptador);
+            adaptador.notifyDataSetChanged();
+        } else {
+            adaptador.actualizarClientes(array_clientes);
+        }
+    }
+
+    @Override
+    public void onSuccess(Cliente cliente) {
+        adaptador.actualizarCliente(cliente);
+    }
+
+    @Override
+    public void onError(String mensaje) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+    public void filtrarClientesDNI(View view) {
+        String DNI = filtroDNI_cliente.getText().toString();
+        if(DNI.isEmpty()) {
+            Toast.makeText(this, "Es obligatorio proporcionar un DNI", Toast.LENGTH_LONG).show();
+        } else if (!Regex.validarDNI(DNI)) {
+            Toast.makeText(this, "El DNI proporcionado no cumple el formato requerido", Toast.LENGTH_LONG).show();
+        }  else {
+            ClienteController.getClientePorDNI(DNI, this, this);
+        }
+
+    }
+
+    public void filtrarClientesNombre(View view) {
+        String nombre = filtroNombre_cliente.getText().toString();
+        if(nombre.isEmpty()) {
+            Toast.makeText(this, "Es obligatorio proporcionar un nombre", Toast.LENGTH_LONG).show();
+        } else if (nombre.length() > 50) {
+            Toast.makeText(this, "El nombre proporcionado no puede tener más de 50 caracteres", Toast.LENGTH_LONG).show();
+        } else {
+            ClienteController.getClientesPorNombre(nombre, this, this);
+        }
+    }
+
+    public void ruedaAjustes(View view) {
+        Intent ajustes = new Intent(this, Ajustes.class);
+        startActivity(ajustes);
+    }
+}
