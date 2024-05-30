f8896e9ba7d2bb7e62bcf901ab896a4a0aeb5db8
ciroco49 <ciroiencom@hotmail.com>
2024-05-30 22:56:34 +0200

    Método API buscar por PK Especialistas

--- ProyectoIntelliJ/tfg/tfg/src/main/java/com/ciroiencom/tfg/repositorio/RepoEspecialista.java
+++ ProyectoIntelliJ/tfg/tfg/src/main/java/com/ciroiencom/tfg/repositorio/RepoEspecialista.java
@@ -2,11 +2,12 @@ package com.ciroiencom.tfg.repositorio;
 
 
 import com.ciroiencom.tfg.modelo.Cliente;
+import com.ciroiencom.tfg.modelo.DTO.EspecialistaDTO;
 import com.ciroiencom.tfg.modelo.Especialista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepoEspecialista extends JpaRepository<Especialista, String> {
-
+    Especialista findByDNI(String DNI);
 
 
 }
